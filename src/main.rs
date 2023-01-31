9bd8f636320c8cd9a188d1de0ce322db4419ff0d
Scott Lambert <s-lambert@live.com.au>
2023-01-31 21:59:27 +0800

    Add player movement

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
 #[derive(Component)]
-struct Velocity(Vec2);
+struct Player;
+
+const PLAYER_SPEED: f32 = 100.0;
 
 fn global_setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
@@ -19,12 +22,15 @@ fn setup_player(
     let texture_atlas =
         TextureAtlas::from_grid(spritesheet_handle, Vec2::new(24.0, 24.0), 4, 1, None, None);
     commands.spawn((
+        Player,
         SpriteSheetBundle {
             texture_atlas: texture_atlases.add(texture_atlas),
-            transform: Transform::from_scale(Vec3::splat(6.0)),
+            transform: Transform::from_scale(Vec3::splat(2.0)),
             ..default()
         },
         AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
+        RigidBody::Dynamic,
+        Velocity::default(),
     ));
 }
 
@@ -46,6 +52,34 @@ fn animate_player(
     }
 }
 
+// Replace with a spring https://theorangeduck.com/page/spring-roll-call
+fn lerp(x: f32, y: f32, t: f32) -> f32 {
+    return (1.0 - t) * x + t * y;
+}
+
+fn move_player(
+    _time: Res<Time>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<&mut Velocity, With<Player>>,
+) {
+    let Some(mut player_velocity) = query.iter_mut().next() else { return; };
+
+    if keyboard_input.pressed(KeyCode::Left) {
+        player_velocity.linvel.x = lerp(player_velocity.linvel.x, -PLAYER_SPEED, 0.5);
+    } else if keyboard_input.pressed(KeyCode::Right) {
+        player_velocity.linvel.x = lerp(player_velocity.linvel.x, PLAYER_SPEED, 0.5);
+    } else {
+        player_velocity.linvel.x = lerp(player_velocity.linvel.x, 0.0, 0.8);
+    }
+    if keyboard_input.pressed(KeyCode::Down) {
+        player_velocity.linvel.y = lerp(player_velocity.linvel.y, -PLAYER_SPEED, 0.5);
+    } else if keyboard_input.pressed(KeyCode::Up) {
+        player_velocity.linvel.y = lerp(player_velocity.linvel.y, PLAYER_SPEED, 0.5);
+    } else {
+        player_velocity.linvel.y = lerp(player_velocity.linvel.y, 0.0, 0.8);
+    }
+}
+
 fn main() {
     App::new()
         .add_plugins(
@@ -61,9 +95,12 @@ fn main() {
                     ..default()
                 }),
         )
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+        .add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(global_setup)
         .add_startup_system(setup_player)
-        .add_system(animate_player)
+        .add_system(move_player)
+        .add_system(animate_player.after(move_player))
         .add_system(bevy::window::close_on_esc)
         .run();
 }
