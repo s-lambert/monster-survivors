49508f3c125f3806875205071a7fcbcdcf7ff3c7
Scott Lambert <s-lambert@live.com.au>
2023-01-31 21:27:32 +0800

    Initialize the window

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use bevy::prelude::*;
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
+#[derive(Component)]
+struct Velocity(Vec2);
+
+fn global_setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
 fn setup_player(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -11,7 +18,6 @@ fn setup_player(
     let spritesheet_handle = asset_server.load("player-sheet.png");
     let texture_atlas =
         TextureAtlas::from_grid(spritesheet_handle, Vec2::new(24.0, 24.0), 4, 1, None, None);
-    commands.spawn(Camera2dBundle::default());
     commands.spawn((
         SpriteSheetBundle {
             texture_atlas: texture_atlases.add(texture_atlas),
@@ -42,7 +48,20 @@ fn animate_player(
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest()))
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    window: WindowDescriptor {
+                        title: "Breakout!".to_string(),
+                        width: 500.0,
+                        height: 500.0,
+                        ..default()
+                    },
+                    ..default()
+                }),
+        )
+        .add_startup_system(global_setup)
         .add_startup_system(setup_player)
         .add_system(animate_player)
         .add_system(bevy::window::close_on_esc)
